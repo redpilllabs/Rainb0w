c67b46e65241c96bbba1429a9fe084267449cc1f
0xLem0nade <18480836+0xLem0nade@users.noreply.github.com>
2022-11-19 04:09:15 +0330

    Add pre-requisites check

---
+++ scripts/00-pre-requisites.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+fn_check_package_installed() {
+    dpkg --status $1 &>/dev/null
+    if [ $? -eq 0 ]; then
+        echo "$1: Already installed"
+    else
+        sudo apt-get install -y $1
+    fi
+}
+
+if [[ $DISTRO =~ "Ubuntu" || $DISTRO =~ "Debian" ]]; then
+    fn_check_package_installed jq
+    fn_check_package_installed libfribidi-bin
+    if [[ ! -f /usr/bin/yq ]]; then
+        sudo wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O /usr/bin/yq &&
+            sudo chmod +x /usr/bin/yq
+    fi
+fi
