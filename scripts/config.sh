08b91a411bf369746eb2f44f04272c58a6cf0bbf
0xLem0nade <18480836+0xLem0nade@users.noreply.github.com>
2023-01-03 18:15:42 +0330

    Update config.sh to hold necessary vars

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,7 +1,34 @@
 #!/bin/bash
 
+VERSION="1.0"
 # Platform
-DISTRO=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
+DISTRO="$(awk -F= '/^NAME/{print $2}' /etc/os-release)"
 
 # Path
-DOCKER_SRC_DIR=$HOME/Docker
+DOCKER_DST_DIR=$HOME/Docker
+DOCKER_SRC_DIR=/tmp/0xLem0nade/Docker
+
+### Server ###
+DOMAIN=""
+EFF_EMAIL=""
+SNI_ARR=()
+# Xray proxies
+XTLS_UUID=$(uuid)
+TROJAN_H2_PASSWORD=$(openssl rand -base64 8)
+TROJAN_GRPC_PASSWORD=$(openssl rand -base64 8)
+TROJAN_GRPC_SERVICENAME=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 5 | head -n 1)
+TROJAN_WS_PASSWORD=$(openssl rand -base64 8)
+TROJAN_WS_PATH=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 5 | head -n 1)
+VMESS_WS_UUID=$(uuid)
+VMESS_WS_PATH=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 5 | head -n 1)
+XTLS_SUBDOMAIN=""
+TROJAN_H2_SUBDOMAIN=""
+TROJAN_GRPC_SUBDOMAIN=""
+TROJAN_WS_SUBDOMAIN=""
+VMESS_WS_SUBDOMAIN=""
+# Hysteria proxy
+HYSTERIA_PASSWORD=$(openssl rand -base64 8)
+HYSTERIA_SUBDOMAIN=""
+# Telegram proxy
+TG_SECRET=$(head -c 16 /dev/urandom | xxd -ps)
+MTPROTO_SUBDOMAIN=""
