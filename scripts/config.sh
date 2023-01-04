76e131c5ac6212ebca7c44eabca8d45a8b90110d
0xLem0nade <18480836+0xLem0nade@users.noreply.github.com>
2023-01-05 02:38:02 +0330

    Make curl call silent

--- scripts/config.sh
+++ scripts/config.sh
@@ -10,7 +10,7 @@ DOCKER_DST_DIR=$HOME/Docker
 DOCKER_SRC_DIR=/tmp/0xLem0nade/Docker
 
 ### Domains ###
-PUBLIC_IP=$(curl icanhazip.com)
+PUBLIC_IP=$(curl -s icanhazip.com)
 DOMAIN=""
 SNI_ARR=()
 # Xray proxies
