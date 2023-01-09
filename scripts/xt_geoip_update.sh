b7f2c70053d7a43ec98ad51b8dabc50df935fd2d
0xLem0nade <18480836+0xLem0nade@users.noreply.github.com>
2023-01-10 03:20:37 +0330

    Update geoip updater script

--- scripts/xt_geoip_update.sh
+++ scripts/xt_geoip_update.sh
@@ -5,27 +5,28 @@ DISTRO_VERSION=$(echo "$(awk -F= '/^VERSION_ID/{print $2}' /etc/os-release)" | t
 MON=$(date +"%m")
 YR=$(date +"%Y")
 
+if [ "$DISTRO_VERSION" == "20.04" ]; then
+    echo -e "${RED}xt_geoip module on Ubuntu 20.04 needs MaxMind database which is no longer available without a license! You need to upgrade to 22.04!"
+    exit 0
+fi
+
 if [ "$(lsmod | grep ^xt_geoip)" ]; then
     # Download the latest GeoIP database
-    sudo mkdir /usr/share/xt_geoip
-    sudo wget "https://download.db-ip.com/free/dbip-country-lite-${YR}-${MON}.csv.gz" -O /usr/share/xt_geoip/dbip-country-lite.csv.gz
-    sudo gunzip /usr/share/xt_geoip/dbip-country-lite.csv.gz
-    # Convert CSV database to binary format for xt_geoip
-    if [[ "$DISTRO" =~ "Ubuntu" ]]; then
-        if (($(echo "$DISTRO_VERSION == 20.04" | bc -l))); then
-            sudo /usr/lib/xtables-addons/xt_geoip_build -D /usr/share/xt_geoip/ -S /usr/share/xt_geoip/
-        elif (($(echo "$DISTRO_VERSION == 22.04" | bc -l))); then
-            sudo /usr/libexec/xtables-addons/xt_geoip_build -s -i /usr/share/xt_geoip/dbip-country-lite.csv.gz
-        fi
-    elif [[ "$DISTRO" =~ "Debian GNU/Linux" ]]; then
-        if (($(echo "$DISTRO_VERSION == 11" | bc -l))); then
-            sudo /usr/libexec/xtables-addons/xt_geoip_build -s -i /usr/share/xt_geoip/dbip-country-lite.csv.gz
-        fi
+    MON=$(date +"%m")
+    YR=$(date +"%Y")
+    if [ ! -d "/usr/share/xt_geoip" ]; then
+        sudo mkdir /usr/share/xt_geoip
     fi
+
+    sudo curl -s "https://download.db-ip.com/free/dbip-country-lite-${YR}-${MON}.csv.gz" >/usr/share/xt_geoip/dbip-country-lite-$YR-$MON.csv.gz
+    sudo gunzip /usr/share/xt_geoip/dbip-country-lite-$YR-$MON.csv.gz
+
+    # Convert CSV database to binary format for xt_geoip
+    sudo /usr/libexec/xtables-addons/xt_geoip_build -s -i /usr/share/xt_geoip/dbip-country-lite-$YR-$MON.csv
 
     # Load xt_geoip kernel module
     modprobe xt_geoip
 
     # Cleanup
-    sudo rm /usr/share/xt_geoip/dbip-country-lite.csv
+    sudo rm /usr/share/xt_geoip/dbip-country-lite-$YR-$MON.csv
 fi
